3c91b2a59ce5554cf7def9a5f4020e3126e479ba
Tamás Krizsán <tamas@krizsanmunka.hu>
2020-10-26 20:04:13 +0100

    feat(function-library): add "7zip.sh" shared script file

---
+++ function-library/7zip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 7-zip shared settings.
+# Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### INCLUDES
+
+source bash.inc.sh
+
+
+### SHARED SETTINGS
+
+## Constants
+
+readonly -- SEVENZIP_SHARED_SETTINGS="-bb3";
+
+readonly -- SEVENZIP_LOG_SUFFIX="7zip_log.log";
