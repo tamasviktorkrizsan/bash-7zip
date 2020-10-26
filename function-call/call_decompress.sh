37b2526e03fae00a55ad4b7830c7715ec12d3cc5
Tamás Krizsán <tamas@krizsanmunka.hu>
2020-10-26 14:52:01 +0100

    feat(function-call): add "call_decompress.sh" function call script

---
+++ function-call/call_decompress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Call decompress function.
+# Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### INCLUDES
+
+source decompress.sh
+
+
+### INPUT PARAMETERS
+
+input="";
+
+
+### FUNCTION CALL
+
+decompress "$input";
+
+next_script "" "";
