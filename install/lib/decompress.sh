bed773834059090658b79a643642f4bdbf8efb62
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-02-10 22:54:02 +0100

    refactor(decompress.sh): change variable names, style, update copyright

--- install/lib/decompress.sh
+++ install/lib/decompress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Decompress archive files.
-# Copyright (C) 2020 Tamas Viktor Krizsan
+# Copyright (C) 2020-2021 Tamas Viktor Krizsan
 # <https://github.com/tamasviktorkrizsan>
 # License: GPL-3.0-or-later
 
@@ -23,7 +23,7 @@ function decompress () {
 
 ## Input Parameters
 
-local user_input_file="${1:-*.7z:*.zip:*.rar:*.iso}";
+local usr_input_file="${1:-*.7z:*.zip:*.rar:*.iso}";
 
 
 ## Constants
@@ -35,15 +35,21 @@ local -r DECOMPRESS_SETTINGS="-y";
 
 ## Variables
 
-local archive_folder_number archive_file_number output_folder output_directory;
+local archive_folder_number;
+
+local archive_file_number;
+
+local output_folder;
+
+local output_dir;
 
 
 ## Processing & Executing Command
 
-for i in $user_input_file;
+for i in $usr_input_file;
 
 
-# count files and foldes
+# count files and folders
 
 do archive_folder_number=$(7z l -slt "$i" | grep 'Path = ' | grep -vEc '(/|7z|\.*$)');
 
@@ -56,7 +62,7 @@ if [[ archive_folder_number -gt 1 ]] || [[ archive_file_number -gt 0 ]];
 
   then output_folder=$(replace_whitespace "${i%.*}");
 
-  output_directory="-o$output_folder";
+  output_dir="-o$output_folder";
 
   else output_directory='';
 fi;
@@ -64,7 +70,7 @@ fi;
 
 # decompress files
 
-7z x $SEVENZIP_SHARED_SETTINGS $DECOMPRESS_SETTINGS "$i" $output_directory 2>&1 |\
+7z x $SEVENZIP_SHARED_SETTINGS $DECOMPRESS_SETTINGS "$i" $output_dir 2>&1 |\
 tee "${i%.*}"_$SEVENZIP_LOG_SUFFIX;
 done;
 
